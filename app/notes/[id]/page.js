2d8e86b0ae0549162986e1862007c2655e4d28c3
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-02-02 20:04:35 +0330

    add note page

---
+++ app/notes/[id]/page.js
@@ -0,0 +1,27 @@
+import styles from "../Notes.module.css";
+
+async function getNote(id) {
+  const res = await fetch(
+    `https://6300a18859a8760a757d441c.mockapi.io/posts/${id}`,
+    {
+      next: { revalidate: 10 },
+    }
+  );
+  const data = await res.json();
+  return data;
+}
+
+export default async function NotePage({ params }) {
+  const note = await getNote(params.id);
+  const { id, title, body, createdAt } = note;
+  return (
+    <div>
+      <h1>notes/{id}</h1>
+      <div className={styles.note}>
+        <h3>{title}</h3>
+        <h5>{body}</h5>
+        <p>{createdAt}</p>
+      </div>
+    </div>
+  );
+}
